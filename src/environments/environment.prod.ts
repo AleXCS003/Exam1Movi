4330efa6913de9ea85f2fee2e70578e3ac9d8687
Alex <alexd003cardenas@gmail.com>
2024-11-22 20:37:04 -0500

    configuracion de credenciales y apk

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase:{
-    apiKey: "AIzaSyAuKtCEdBHIp1OPAchWLPwBJD94xGEz9bw",
-    authDomain: "b-dfe16.firebaseapp.com",
-    projectId: "b-dfe16",
-    storageBucket: "b-dfe16.firebasestorage.app",
-    messagingSenderId: "167089758686",
-    appId: "1:167089758686:web:c94d24ca8d8a3e920e7d4f",
-    measurementId: "G-LTD6KKYYGM"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
     }
 };
